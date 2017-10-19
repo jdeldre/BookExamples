2febdcaa5dae363e5fd86444ed2b7b428c099d52
Jeff Eldredge <jdeldre@gmail.com>
2017-10-19 13:03:49 -0700

    Added uniform flow to streamfunction calculation about bodies. Created general PlotStyle structure for uniform plots in book examples

--- src/BookExamples.jl
+++ src/BookExamples.jl
@@ -11,19 +11,42 @@ import PyCall
 export plot_streamlines, conformal_grid
 
 const mygreen = [151/255,180/255,118/255];
-const gxicolor = [113/255,161/255,103/255];
-const getacolor = [74/255,144/255,226/255];
+const mygreen2 = [113/255,161/255,103/255];
+const myblue = [74/255,144/255,226/255];
 
-function plot_style()
+PlotColor = Union{Vector{Float64},String,Symbol}
+
+struct PlotStyle
+
+  bodycolor :: PlotColor
+  xicolor   :: PlotColor
+  etacolor  :: PlotColor
+  markercolor :: PlotColor
+  linewidth :: Int64
+  pointsize :: Int64
+
+end
+
+
+function PlotStyle()
   PyCall.PyDict(matplotlib["rcParams"])["font.family"] = "STIXGeneral";
   PyCall.PyDict(matplotlib["rcParams"])["mathtext.fontset"] = "stix";
   PyCall.PyDict(matplotlib["rcParams"])["font.size"] = 10;
+
+  bodycolor = mygreen
+  xicolor = mygreen2
+  etacolor = myblue
+  markercolor = "r"
+  linewidth = 1
+  pointsize = 3
+
+  PlotStyle(bodycolor,xicolor,etacolor,markercolor,linewidth,pointsize)
 
 end
 
 function conformal_grid(b::Bodies.PowerBody)
 
-  plot_style()
+  ps = PlotStyle()
 
   xmaxc = 4.0
   rmax = sqrt(2)*xmaxc
@@ -52,10 +75,10 @@ function conformal_grid(b::Bodies.PowerBody)
       zg = Bodies.powermap(ζg,b.C)
 
        PyPlot.subplot(121)
-       PyPlot.plot(real(ζg),imag(ζg),linewidth=1,color=gxicolor)
+       PyPlot.plot(real(ζg),imag(ζg),linewidth=ps.linewidth,color=ps.xicolor)
 
        PyPlot.subplot(122)
-       PyPlot.plot(real(zg),imag(zg),linewidth=1,color=gxicolor)
+       PyPlot.plot(real(zg),imag(zg),linewidth=ps.linewidth,color=ps.xicolor)
 
        θg += dθ
    end
@@ -77,10 +100,10 @@ function conformal_grid(b::Bodies.PowerBody)
        zg = Bodies.powermap(ζg,b.C)
 
        PyPlot.subplot(121)
-       PyPlot.plot(real(ζg),imag(ζg),linewidth=1,color=getacolor)
+       PyPlot.plot(real(ζg),imag(ζg),linewidth=ps.linewidth,color=ps.etacolor)
 
        PyPlot.subplot(122)
-       PyPlot.plot(real(zg),imag(zg),linewidth=1,color=getacolor)
+       PyPlot.plot(real(zg),imag(zg),linewidth=ps.linewidth,color=ps.etacolor)
 
        rg += dr
    end
@@ -122,14 +145,7 @@ function setup_grid(b::Bodies.PowerBody)
 
 end
 
-function plot_streamlines(b::Bodies.PowerBody,m::Bodies.RigidBodyMotion,t)
-
-  plot_style()
-
-  Z, ζ = setup_grid(b)
-
-  ψ = zero(ζ);
-  ψ = Bodies.streamfunction.(ζ,b,m,t)
+function set_contour_levels(ψ)
 
   vmin = minimum(ψ);
   vmax = maximum(ψ);
@@ -141,10 +157,25 @@ function plot_streamlines(b::Bodies.PowerBody,m::Bodies.RigidBodyMotion,t)
   if (sign(vmin) != sign(vmax))
     v = v .- v[abs.(v).==minimum(abs.(v))];
   end
+  v
 
-  pc = PyPlot.contour(real(Z),imag(Z),ψ,v,colors="black",linestyles="solid",linewidths=1);
-  pf = PyPlot.fill(real(b.zs),imag(b.zs),facecolor=mygreen);
-  PyPlot.plot(real(b.zs),imag(b.zs),color=mygreen);
+end
+
+function plot_streamlines(b::Bodies.PowerBody,m::Bodies.RigidBodyMotion,t)
+
+  ps = PlotStyle()
+
+  Z, ζ = setup_grid(b)
+
+  ψ = zero(ζ);
+  ψ = Bodies.streamfunction.(ζ,b,m,t)
+
+
+
+  pc = PyPlot.contour(real(Z),imag(Z),ψ,set_contour_levels(ψ),
+                      colors="black",linestyles="solid",linewidths=ps.linewidth);
+  pf = PyPlot.fill(real(b.zs),imag(b.zs),facecolor=ps.bodycolor);
+  PyPlot.plot(real(b.zs),imag(b.zs),color=ps.bodycolor);
 
   #PyPlot.savefig("/Users/jeff/Desktop/testfig2.eps",format="eps")
 
@@ -152,30 +183,42 @@ end
 
 function plot_streamlines(b::Bodies.PowerBody,src,t)
 
-  plot_style()
+  ps = PlotStyle()
 
   Z, ζ = setup_grid(b)
 
-  #ψ = zero(ζ);
   ψ = [Bodies.streamfunction(ζi,b,src,t) for ζi in ζ]
 
-  vmin = minimum(ψ);
-  vmax = maximum(ψ);
-  vmean = vmax-vmin;
-  vfact = 1;
-  vmin = vmean+vfact*(vmin-vmean);
-  vmax = vmean+vfact*(vmax-vmean);
-  v = linspace(vmin,vmax,31);
-  if (sign(vmin) != sign(vmax))
-    v = v .- v[abs.(v).==minimum(abs.(v))];
-  end
 
-  pc = PyPlot.contour(real(Z),imag(Z),ψ,v,colors="black",linestyles="solid",linewidths=1);
-  pf = PyPlot.fill(real(b.zs),imag(b.zs),facecolor=mygreen);
-  PyPlot.plot(real(b.zs),imag(b.zs),color=mygreen);
+  pc = PyPlot.contour(real(Z),imag(Z),ψ,set_contour_levels(ψ),
+                      colors="black",linestyles="solid",linewidths=ps.linewidth);
+  pf = PyPlot.fill(real(b.zs),imag(b.zs),facecolor=ps.bodycolor);
+  PyPlot.plot(real(b.zs),imag(b.zs),color=ps.bodycolor);
 
   z = Elements.conftransform(src,b);
-  PyPlot.plot(real.(z),imag.(z),"ro",markersize=3);
+  PyPlot.plot(real.(z),imag.(z),
+              marker="o",markerfacecolor=ps.markercolor,markersize=ps.pointsize,
+              markeredgecolor=ps.markercolor,linestyle="none");
+
+  #PyPlot.savefig("/Users/jeff/Desktop/testfig2.eps",format="eps")
+
+end
+
+function plot_streamlines(b::Bodies.PowerBody,Winf::Complex128)
+
+  ps = PlotStyle()
+
+  Z, ζ = setup_grid(b)
+
+  ψ = zero(ζ);
+  ψ = Bodies.streamfunction.(ζ,b,Winf,0.0)
+
+
+
+  pc = PyPlot.contour(real(Z),imag(Z),ψ,set_contour_levels(ψ),
+                      colors="black",linestyles="solid",linewidths=ps.linewidth);
+  pf = PyPlot.fill(real(b.zs),imag(b.zs),facecolor=ps.bodycolor);
+  PyPlot.plot(real(b.zs),imag(b.zs),color=ps.bodycolor);
 
   #PyPlot.savefig("/Users/jeff/Desktop/testfig2.eps",format="eps")
 
