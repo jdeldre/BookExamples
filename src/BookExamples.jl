9f4d81b8fb326fa66576d5279b0a2ab630615d9f
Jeff Eldredge <jdeldre@gmail.com>
2017-10-18 22:06:42 -0700

    Added grid lines plotting for conformal mapping

--- src/BookExamples.jl
+++ src/BookExamples.jl
@@ -8,10 +8,84 @@ import PotentialFlow.Utils:@get, MappedVector
 
 import PyCall
 
-export plot_streamlines
+export plot_streamlines, conformal_grid
 
 const mygreen = [151/255,180/255,118/255];
+const gxicolor = [113/255,161/255,103/255];
+const getacolor = [74/255,144/255,226/255];
 
+function plot_style()
+  PyCall.PyDict(matplotlib["rcParams"])["font.family"] = "STIXGeneral";
+  PyCall.PyDict(matplotlib["rcParams"])["mathtext.fontset"] = "stix";
+  PyCall.PyDict(matplotlib["rcParams"])["font.size"] = 10;
+
+end
+
+function conformal_grid(b::Bodies.PowerBody)
+
+  plot_style()
+
+  xmaxc = 4.0
+  rmax = sqrt(2)*xmaxc
+
+  PyPlot.subplot(121)
+  PyPlot.axis("scaled")
+  PyPlot.axis([-xmaxc,xmaxc,-xmaxc,xmaxc])
+  PyPlot.xticks(ceil(-xmaxc/2)*2:2:floor(xmaxc/2)*2)
+  PyPlot.yticks(ceil(-xmaxc/2)*2:2:floor(xmaxc/2)*2)
+
+  PyPlot.subplot(122)
+  PyPlot.axis("scaled")
+  xmaxp = xmaxc*abs(b.C[1])
+  dxp = 0.5*xmaxp
+  PyPlot.axis([-xmaxp,xmaxp,-xmaxp,xmaxp])
+  PyPlot.xticks(ceil(-xmaxp/dxp)*dxp:dxp:floor(xmaxp/dxp)*dxp)
+  PyPlot.yticks(ceil(-xmaxp/dxp)*dxp:dxp:floor(xmaxp/dxp)*dxp)
+
+  nθ = 20
+  dθ = 2π/nθ
+  θg = 0
+  nrg = 100
+  rg = linspace(1,rmax,nrg)
+  for jθ in 1:nθ
+      ζg = collect(rg*exp(im*θg))
+      zg = Bodies.powermap(ζg,b.C)
+
+       PyPlot.subplot(121)
+       PyPlot.plot(real(ζg),imag(ζg),linewidth=1,color=gxicolor)
+
+       PyPlot.subplot(122)
+       PyPlot.plot(real(zg),imag(zg),linewidth=1,color=gxicolor)
+
+       θg += dθ
+   end
+
+   nr = 10
+   dr = (rmax-1)/nr
+   rg = 1
+
+   nθg = 200
+   θg = linspace(0,2π,nθg)
+   #if strcmpi(maptype,'sc')
+  #     thg = [thg shape.prevangle'];  % Include prevertices
+  #     thsort = sort(thg - floor(thg/(2*pi))*2*pi);  % Normalize the range
+    #   thg = thsort(abs(diff(thsort))>1e-15);  % Eliminate duplicates
+  #     thg = [thg 2*pi];
+   #end
+   for jr in 1:nr
+       ζg = collect(rg*exp.(im*θg));
+       zg = Bodies.powermap(ζg,b.C)
+
+       PyPlot.subplot(121)
+       PyPlot.plot(real(ζg),imag(ζg),linewidth=1,color=getacolor)
+
+       PyPlot.subplot(122)
+       PyPlot.plot(real(zg),imag(zg),linewidth=1,color=getacolor)
+
+       rg += dr
+   end
+
+end
 
 function setup_grid(b::Bodies.PowerBody)
 
@@ -50,9 +124,7 @@ end
 
 function plot_streamlines(b::Bodies.PowerBody,m::Bodies.RigidBodyMotion,t)
 
-  PyCall.PyDict(matplotlib["rcParams"])["font.family"] = "STIXGeneral";
-  PyCall.PyDict(matplotlib["rcParams"])["mathtext.fontset"] = "stix";
-  PyCall.PyDict(matplotlib["rcParams"])["font.size"] = 14;
+  plot_style()
 
   Z, ζ = setup_grid(b)
 
@@ -80,9 +152,7 @@ end
 
 function plot_streamlines(b::Bodies.PowerBody,src,t)
 
-  PyCall.PyDict(matplotlib["rcParams"])["font.family"] = "STIXGeneral";
-  PyCall.PyDict(matplotlib["rcParams"])["mathtext.fontset"] = "stix";
-  PyCall.PyDict(matplotlib["rcParams"])["font.size"] = 14;
+  plot_style()
 
   Z, ζ = setup_grid(b)
 
