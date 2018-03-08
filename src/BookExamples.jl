745a20328a7558dfb6d95be8ee68a0648ddebc5c
Jeff Eldredge <jdeldre@gmail.com>
2018-03-08 12:19:38 -0800

    Corrected a small bug in set_contour_levels

--- src/BookExamples.jl
+++ src/BookExamples.jl
@@ -127,6 +127,38 @@ function conformal_grid(b::Bodies.PowerBody)
 
 end
 
+function setup_grid()
+
+  len = 1;
+  xcent = 0; ycent = 0;
+  xfact = 6; yfact = 6;
+  xlen = xfact*len; ylen = yfact*len;
+  xmin = xcent - 0.5*xlen; xmax = xcent + min(0.5*xlen,len);
+  ymin = ycent - min(0.5*ylen,2*len);
+  ymax = ycent + min(0.5*ylen,2*len);
+
+  nth = 400;
+  dth = 2π/nth;
+  θ = linspace(dth,2π+dth,nth+1);
+  dr = dth;
+  r = [dr];
+  while maximum(r) < xlen
+    push!(r,r[end]+dr)
+    dr = r[end]*dth;
+  end
+  xmin = -maximum(r)/sqrt(2); xmax = maximum(r)/sqrt(2);
+  ymin = -maximum(r)/sqrt(2); ymax = maximum(r)/sqrt(2);
+  Z = r * exp.(im*θ');
+
+  PyPlot.axis("scaled");
+  PyPlot.axis([xmin,xmax,ymin,ymax]);
+  PyPlot.xticks(ceil(xmin/2)*2:2:floor(xmax/2)*2);
+  PyPlot.yticks(ceil(ymin/2)*2:2:floor(ymax/2)*2);
+
+  Z
+
+end
+
 function setup_grid(b::Bodies.PowerBody)
 
   eps = 0.00001;
@@ -162,19 +194,52 @@ function setup_grid(b::Bodies.PowerBody)
 
 end
 
-function set_contour_levels(ψ)
+function set_contour_levels(ψ,vmin::Float64,vmax::Float64,n::Int64)
 
-  vmin = minimum(ψ);
-  vmax = maximum(ψ);
   vmean = vmax-vmin;
   vfact = 1;
   vmin = vmean+vfact*(vmin-vmean);
   vmax = vmean+vfact*(vmax-vmean);
-  v = linspace(vmin,vmax,31);
+  v = linspace(vmin,vmax,n);
+  return v
+
+end
+
+function set_contour_levels(ψ)
+
+  vmin = minimum(ψ);
+  vmax = maximum(ψ);
+  v = set_contour_levels(ψ,vmin,vmax,31)
   if (sign(vmin) != sign(vmax))
     v = v .- v[abs.(v).==minimum(abs.(v))];
   end
-  v
+  return v
+
+end
+
+function plot_streamlines(a::Vector{Complex128})
+
+  ps = PlotStyle()
+  Z = setup_grid()
+
+  F = a[1]*Z
+  if length(a) > 1
+    F += a[2]*log.(Z)
+  end
+  Z⁻ⁿ = 1./Z
+  for j = 3:length(a)
+    F += a[j]*Z⁻ⁿ
+    Z⁻ⁿ ./= Z
+  end
+  ψ = imag(F)
+
+  ψmin = minimum(ψ[abs.(Z).>1.0])
+  ψmax = maximum(ψ[abs.(Z).>1.0])
+
+
+  pc = PyPlot.contour(real(Z),imag(Z),ψ,
+                      set_contour_levels(ψ,ψmin,ψmax,21),
+                      colors="black",linestyles="solid",linewidths=ps.linewidth);
 
 end
 
